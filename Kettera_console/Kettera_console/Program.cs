c26ce5dd70386a99e0fcb670f1e2d90bf05900fa
JereAmpuja <jere.ampuja@student.lab.fi>
2024-11-19 16:04:52 +0200

    dbConnection
    Aloitin työstämään yhteyttä tietokantaan. Melko aluillaan vielä. Tein vasta luokan ja muutamat perus hommat.

--- Kettera_console/Kettera_console/Program.cs
+++ Kettera_console/Kettera_console/Program.cs
@@ -3,9 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
 
 namespace Kettera_console
 {
+    public class dbConnection //Luokka jolla yhdistetään DATABASEEN.
+    {
+        string connectionString = @"Kettera_console/Data/Kettera.accdb"; //Tietokannan sijainti (ainakin pitäisi olla XD).
+        public dbConnection()
+        {
+            OleDbConnection myConnection = new OleDbConnection(); //luo OleDbConnection tyyppisen olion.
+            myConnection.ConnectionString = connectionString; //Asettaa connectionstringin tietokannan sijainniksi.
+            myConnection.Open(); //Avaa yhteyden.
+
+        
+            //luodaan komento.
+            OleDbCommand myCommand = new OleDbCommand(); //luo OleDbCommand tyyppisen olion. Tällä tehdään muokkaukset tietokantaan.
+            myCommand.Connection = myConnection;
+
+            // Komentoa muutetaan tällä tavalla:
+            // myCommand.CommandText = "INSERT INTO Taulu (Kenttä1, Kenttä2) VALUES ('Arvo1', 'Arvo2')";
+            // myCommand.CommandType = CommandType.Text; //Tämä kertoo että kyseessä on tekstikomento.
+        }
+
+        
+
+    }
     internal class Program
     {
         static void Main(string[] args)
