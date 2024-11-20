67ac3300e50614992655607113521cbd86dc8b3e
JereAmpuja <jere.ampuja@student.lab.fi>
2024-11-20 17:29:17 +0200

    Tiedon haun edistystä
    Tein muutaman metodin ja pari luokkaa.

--- Kettera_console/Kettera_console/Program.cs
+++ Kettera_console/Kettera_console/Program.cs
@@ -13,16 +13,23 @@ namespace Kettera_console
 {
     public class dbConnection //Luokka jolla yhdistetään DATABASEEN.
     {
-        string appLocation = AppDomain.CurrentDomain.BaseDirectory; //Tämä hakee ohjelman sijainnin.
-        string databasePath; //koko tietokannan sijainti tulee tähän rakentajassa.
+        private string appLocation = AppDomain.CurrentDomain.BaseDirectory; //Tämä hakee ohjelman sijainnin.
+        private string databasePath; //koko tietokannan sijainti tulee tähän rakentajassa.
+        public OleDbConnection myConnection;
+        public OleDbCommand myCommand;
+        public OleDbDataReader myReader;
+
+        public List<Customer> customers = new List<Customer>();
+        public List<PersonalTrainer> personalTrainers = new List<PersonalTrainer();
 
         public dbConnection()
         {
 
             databasePath = Path.GetFullPath(Path.Combine(appLocation, @"..\..\..", @"Kettera_console\Data\Kettera.accdb"));
 
-            // Testaa tulosteella
-            Console.WriteLine("Tietokannan polku: " + databasePath);
+            // Testaa löytyykö tiedosto ja tulostaa tuloksen, kommentoitu pois koska homma toimii toistaiseksi.
+            /*
+             * Console.WriteLine("Tietokannan polku: " + databasePath);
             if (!File.Exists(databasePath))
             {
                 Console.WriteLine("Tietokantatiedostoa ei löydy: " + databasePath);
@@ -32,26 +39,95 @@ namespace Kettera_console
             {
                 Console.WriteLine("Tietokantatiedosto löytyi: " + databasePath);
             }
+            */
+
             databasePath = "Provider = Microsoft.ACE.OLEDB.12.0;" + @"Data Source = " + databasePath;
 
-            OleDbConnection myConnection = new OleDbConnection(); //luo OleDbConnection tyyppisen olion.
+            myConnection = new OleDbConnection(); //luo OleDbConnection tyyppisen olion.
             myConnection.ConnectionString = databasePath; //Asettaa connectionstringin tietokannan sijainniksi.         
-            myConnection.Open(); //Avaa yhteyden.
+            // myConnection.Open(); //Avaa yhteyden. KOMMENTOITU POIS KOSKA TOISTAISEKSI TOTEUTUS MUUALLA.
 
 
         
             //luodaan komento.
-            OleDbCommand myCommand = new OleDbCommand(); //luo OleDbCommand tyyppisen olion. Tällä tehdään muokkaukset tietokantaan.
-            myCommand.Connection = myConnection;
+            myCommand = new OleDbCommand(); //luo OleDbCommand tyyppisen olion. Tällä tehdään muokkaukset tietokantaan.
+            myCommand.Connection = myConnection; //yhdistää komennon yhteyteen! :D
+
+            myReader = myCommand.ExecuteReader();
 
             // Komentoa muutetaan tällä tavalla:
             // myCommand.CommandText = "INSERT INTO Taulu (Kenttä1, Kenttä2) VALUES ('Arvo1', 'Arvo2')";
             // myCommand.CommandType = CommandType.Text; //Tämä kertoo että kyseessä on tekstikomento.
         }
 
+        //TÄSTÄ ETEENPÄIN METODIT JOILLA VOIDAAN TEHDÄ KOMENTOJA TIETOKANTAAN.
+
+        public bool GetInfo()
+        {
+            //Ensin haetaan PT tiedot.
+            myConnection.Open();
+            bool notEoF = myReader.Read();
+            myCommand.CommandText = "SELECT * FROM Valmentaja";
+            myCommand.CommandType = CommandType.Text;
+
+            while (notEoF)
+            {
+                PersonalTrainer pt = new PersonalTrainer();
+                pt.ID = myReader.GetInt32(0);
+                pt.Name = myReader.GetString(1);
+                personalTrainers.Add(pt);
+            }
+
+
+            //Seuraavaksi haetaan asiakkaat.
+            myCommand.CommandText = "SELECT * FROM Asiakas";
+            myCommand.CommandType = CommandType.Text;
+            //Ja tehdään tiedoista customer olioita.
+
+            while (notEoF)
+            {                 
+                Customer c = new Customer();
+                c.ID = myReader.GetInt32(0);
+                c.Name = myReader.GetString(1);
+                c.BirthDay = myReader.GetDateTime(2);             
+                c.PersonalTrainerName = myReader.GetString(3);
+                c.GymVisits = myReader.GetInt32(4);
+                c.GroupVisits = myReader.GetInt32(5);
+                c.MembershipEndDay = myReader.GetDateTime(6);
+                customers.Add(c);
+            }
+
+        }
+        public bool AddCustomer(string name, DateTime bDay, string ptName, DateTime membershipEndDay)
+        { }
+
         
 
     }
+
+    public class Lists
+    {
+        public List<Customer> customers = new List<Customer>();
+        public List<PersonalTrainer> personalTrainers = new List<PersonalTrainer>();
+    }
+
+    public class PersonalTrainer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDay { get; set; }
+        public DateTime MembershipEndDay { get; set; }
+        public string PersonalTrainerName { get; set; }
+        public int GymVisits { get; set; }
+        public int GroupVisits { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
